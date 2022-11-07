1cffcc5db38e16a8097ecb093333827d4da21a8d
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-11-07 17:17:39 -0600

    workbook 2.2 starter added

--- Section 8 MVC/workbook2.1/src/main/java/com/ltp/workbook21/ShowsController.java
+++ Section 8 MVC/workbook2.1/src/main/java/com/ltp/workbook21/ShowsController.java
@@ -1,9 +1,8 @@
 package com.ltp.workbook21;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class ShowsController {
-    @GetMapping("/shows")
+
 }
