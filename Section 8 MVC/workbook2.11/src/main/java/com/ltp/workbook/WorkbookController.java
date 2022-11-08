4806b9de12952365c93c3e40b1a0616131f5cf45
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-11-08 16:56:18 -0600

    Workbook 2.11 - create record class - create arraylist of records - add records as model attribute

--- Section 8 MVC/workbook2.11/src/main/java/com/ltp/workbook/WorkbookController.java
+++ Section 8 MVC/workbook2.11/src/main/java/com/ltp/workbook/WorkbookController.java
@@ -2,13 +2,24 @@ package com.ltp.workbook;
 
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Controller
 public class WorkbookController {
-
+    List<Record> records = Arrays.asList(
+            new Record("Chair", 20.99, 5.99),
+            new Record("Table", 40.99, 8.99),
+            new Record("Couch", 100.99, 105.99),
+            new Record("Fridge", 200.99, 59.99),
+            new Record("Bed", 150.99, 205.99)
+    );
     @GetMapping(value="/")
-    public String getSales() {
+    public String getSales(Model model) {
+        model.addAttribute("records", records);
         return "records";
     }
 
