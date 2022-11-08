1c583304e6096371be4fe1f833cc673ae2e532f0
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-11-08 17:05:55 -0600

    Workbook 2.11 - change fields from double to BigDecimal - a better representation of dollar amounts

--- Section 8 MVC/workbook2.11/src/main/java/com/ltp/workbook/WorkbookController.java
+++ Section 8 MVC/workbook2.11/src/main/java/com/ltp/workbook/WorkbookController.java
@@ -5,17 +5,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.math.BigDecimal;
 import java.util.Arrays;
 import java.util.List;
 
 @Controller
 public class WorkbookController {
     List<Record> records = Arrays.asList(
-            new Record("Chair", 20.99, 5.99),
-            new Record("Table", 40.99, 8.99),
-            new Record("Couch", 100.99, 105.99),
-            new Record("Fridge", 200.99, 59.99),
-            new Record("Bed", 150.99, 205.99)
+            new Record("Chair", new BigDecimal("20.99"),  new BigDecimal("5.99")),
+            new Record("Table",  new BigDecimal("40.99"),  new BigDecimal("8.99")),
+            new Record("Couch",  new BigDecimal("100.99"),  new BigDecimal("105.99")),
+            new Record("Fridge",  new BigDecimal("200.99"),  new BigDecimal("59.99")),
+            new Record("Bed",  new BigDecimal("150.99"),  new BigDecimal("205.99"))
     );
     @GetMapping(value="/")
     public String getSales(Model model) {
