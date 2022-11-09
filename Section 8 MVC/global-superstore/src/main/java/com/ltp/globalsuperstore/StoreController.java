3d31d3451be5efb37f935590877a19f7410803da
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-11-09 15:03:07 -0600

    Challenge 2: Global Superstore - Part 1 complete

--- Section 8 MVC/global-superstore/src/main/java/com/ltp/globalsuperstore/StoreController.java
+++ Section 8 MVC/global-superstore/src/main/java/com/ltp/globalsuperstore/StoreController.java
@@ -7,8 +7,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class StoreController {
     @GetMapping("/")
-    public String getForm (Model model) {
+    public String getForm(Model model) {
         model.addAttribute("categories", Constants.CATEGORIES);
         return "form";
+    }
+
+    @GetMapping("/inventory")
+    public String getInventory(Model model) {
+        return "inventory";
     }
 }
