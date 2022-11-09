3499396877d46ead4e48149ee61629b95d4bc484
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-11-09 13:04:30 -0600

    Update student grade lecture complete.

--- Section 8 MVC/grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
+++ Section 8 MVC/grade-submission/src/main/java/com/ltp/gradesubmission/GradeController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,14 +21,29 @@ public class GradeController {
         return "grades";
 
     }
+
     @GetMapping("/")
-    public String gradeForm (Model model) {
-        model.addAttribute("grade", new Grade());
+    public String gradeForm(Model model, @RequestParam(required = false) String name) {
+        Grade grade;
+        model.addAttribute("grade", getGradeIndex(name) == -1000 ? new Grade() : studentGrades.get(getGradeIndex(name)));
         return "form";
     }
+
     @PostMapping("/handleSubmit")
     public String submitGradeForm(Grade grade) {
-        studentGrades.add(grade);
+        int index = getGradeIndex(grade.getName());
+        if (index == -1000) {
+            studentGrades.add(grade);
+        } else {
+            studentGrades.set(index, grade);
+        } ;
         return "redirect:/grades";
+    }
+
+    public Integer getGradeIndex(String name) {
+        for (int i = 0; i < studentGrades.size(); i++) {
+            if (studentGrades.get(i).getName().equals(name)) return i;
+        }
+        return -1000;
     }
 }
