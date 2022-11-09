a0b470e5a481c69954bb5daaf1ba068db931b21f
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-11-09 15:31:11 -0600

    Challenge 2: Global Superstore - Part 2 complete

--- Section 8 MVC/global-superstore/src/main/java/com/ltp/globalsuperstore/StoreController.java
+++ Section 8 MVC/global-superstore/src/main/java/com/ltp/globalsuperstore/StoreController.java
@@ -3,17 +3,28 @@ package com.ltp.globalsuperstore;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class StoreController {
+    private List<Item> items = new ArrayList<>();
     @GetMapping("/")
     public String getForm(Model model) {
         model.addAttribute("categories", Constants.CATEGORIES);
+        model.addAttribute("item", new Item());
         return "form";
     }
 
     @GetMapping("/inventory")
     public String getInventory(Model model) {
         return "inventory";
+    }
+@PostMapping("/submitItem")
+    public String submitOrderForm (Item item) {
+        items.add(item);
+        return "redirect:/inventory";
     }
 }
