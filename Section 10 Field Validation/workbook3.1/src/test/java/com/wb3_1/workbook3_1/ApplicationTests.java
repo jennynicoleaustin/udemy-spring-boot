0371925a51089a976a5a9949850ccb589a459c6e
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-11-12 16:03:03 -0600

    Section 10: Field Validation - Workbook 3.1 added

---
+++ Section 10 Field Validation/workbook3.1/src/test/java/com/wb3_1/workbook3_1/ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.wb3_1.workbook3_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
