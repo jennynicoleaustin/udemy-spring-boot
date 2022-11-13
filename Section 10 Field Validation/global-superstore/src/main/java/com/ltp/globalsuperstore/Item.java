86b40d28177717fb72af5502822188133194582b
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-11-13 16:04:47 -0600

    Challenge 3: - Add validation fields - change html drop down to hardcode categories so that the category selected persists if error

--- Section 10 Field Validation/global-superstore/src/main/java/com/ltp/globalsuperstore/Item.java
+++ Section 10 Field Validation/global-superstore/src/main/java/com/ltp/globalsuperstore/Item.java
@@ -1,15 +1,24 @@
 package com.ltp.globalsuperstore;
 
+import org.springframework.format.annotation.DateTimeFormat;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Past;
 import java.util.Date;
 import java.util.UUID;
 
-import org.springframework.format.annotation.DateTimeFormat;
-
 public class Item {
+    @NotBlank(message = "Please choose a category")
     private String category;
+    @NotBlank(message = "Name cannot be blank")
     private String name;
+    @Min(message = "price must be greater than $0", value = 0L)
     private Double price;
+    @Min(message = "discount must be greater than $0", value = 0L)
+
     private Double discount;
+    @Past(message = "Date must be in the past")
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date date;
     private String id;
--- Section 10 Field Validation/global-superstore/src/main/java/com/ltp/globalsuperstore/StoreController.java
+++ Section 10 Field Validation/global-superstore/src/main/java/com/ltp/globalsuperstore/StoreController.java
@@ -1,16 +1,18 @@
 package com.ltp.globalsuperstore;
 
-import java.util.ArrayList;
-import java.util.Date;
-import java.util.List;
-import java.util.concurrent.TimeUnit;
-
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 @Controller
 public class StoreController {
@@ -21,12 +23,12 @@ public class StoreController {
     public String getForm(Model model, @RequestParam(required = false) String id) {
         int index = getIndexFromId(id);
         model.addAttribute("item", index == Constants.NOT_FOUND ? new Item() : items.get(index));
-        model.addAttribute("categories", Constants.CATEGORIES);
         return "form";
     }
 
     @PostMapping("/submitItem")
-    public String handleSubmit(Item item, RedirectAttributes redirectAttributes) {
+    public String handleSubmit(@Valid Item item, BindingResult result, RedirectAttributes redirectAttributes) {
+        if (result.hasErrors()) return "form";
         int index = getIndexFromId(item.getId());
         String status = Constants.SUCCESS_STATUS;
         if (index == Constants.NOT_FOUND) {
