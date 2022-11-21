8a5be7b65ff23773f125e7f717d79ce17ffa966d
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-11-21 14:36:06 -0600

    Challenge 6 - Add Operation tag

--- Section 19 REST API/ChallengeOpenAPI/src/main/java/com/ltp/contacts/web/ContactController.java
+++ Section 19 REST API/ChallengeOpenAPI/src/main/java/com/ltp/contacts/web/ContactController.java
@@ -2,6 +2,11 @@ package com.ltp.contacts.web;
 
 import com.ltp.contacts.pojo.Contact;
 import com.ltp.contacts.service.ContactService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.ArraySchema;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -14,20 +19,23 @@ import java.util.List;
 @RestController
 @Tag(name = "Contact Controller", description = "add, modify, retrieve contacts")
 public class ContactController {
-    
+
     @Autowired
     private ContactService contactService;
+    @Operation(summary = "Retrieves contacts", description = "Provides a list of all contacts in the db")
+    @ApiResponse(responseCode = "200", description = "Successful retrieval of contacts", content = @Content(array = @ArraySchema(schema = @Schema(implementation = Contact.class))))
     @GetMapping(value = "/contact/all")
     public ResponseEntity<List<Contact>> getContacts() {
         List<Contact> contacts = contactService.getContacts();
         return new ResponseEntity<>(contacts, HttpStatus.OK);
     }
+    @Operation(summary = "Get contact by ID", description = "Returns a contact based on an ID ")
     @GetMapping(value = "/contact/{id}")
     public ResponseEntity<Contact> getContact(@PathVariable String id) {
         Contact contact = contactService.getContactById(id);
         return new ResponseEntity<>(contact, HttpStatus.OK);
     }
-
+    @Operation(summary = "Create contact", description = "Creates a contact from the provided payload")
     @PostMapping(value = "/contact")
     public ResponseEntity<Contact> createContact(@Valid @RequestBody Contact contact) {
         contactService.saveContact(contact);
