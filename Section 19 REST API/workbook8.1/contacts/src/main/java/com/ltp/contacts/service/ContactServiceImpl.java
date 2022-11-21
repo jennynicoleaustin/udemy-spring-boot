83b0fe7cbd23e719dd4d1a635548be00ac75ba6b
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-11-21 10:30:06 -0600

    Workbook 8.1 Complete

--- Section 19 REST API/workbook8.1/contacts/src/main/java/com/ltp/contacts/service/ContactServiceImpl.java
+++ Section 19 REST API/workbook8.1/contacts/src/main/java/com/ltp/contacts/service/ContactServiceImpl.java
@@ -1,13 +1,13 @@
 package com.ltp.contacts.service;
 
-import java.util.List;
-import java.util.stream.IntStream;
-
+import com.ltp.contacts.exception.NoContactException;
+import com.ltp.contacts.pojo.Contact;
+import com.ltp.contacts.repository.ContactRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.ltp.contacts.pojo.Contact;
-import com.ltp.contacts.repository.ContactRepository;
+import java.util.List;
+import java.util.stream.IntStream;
 
 @Service
 public class ContactServiceImpl implements ContactService {
@@ -16,7 +16,7 @@ public class ContactServiceImpl implements ContactService {
     private ContactRepository contactRepository;
 
     @Override
-    public Contact getContactById(String id) {
+    public Contact getContactById(String id) throws NoContactException {
         return contactRepository.getContact(findIndexById(id));
     }
 
@@ -26,12 +26,12 @@ public class ContactServiceImpl implements ContactService {
     }
 
     @Override
-    public void updateContact(String id, Contact contact) {
+    public void updateContact(String id, Contact contact) throws NoContactException {
         contactRepository.updateContact(findIndexById(id), contact);
     }
 
     @Override
-    public void deleteContact(String id) {
+    public void deleteContact(String id) throws NoContactException {
         contactRepository.deleteContact(findIndexById(id)); 
     }
 
@@ -41,11 +41,11 @@ public class ContactServiceImpl implements ContactService {
     }
 
 
-    private int findIndexById(String id) {
+    private int findIndexById(String id) throws NoContactException {
         return IntStream.range(0, contactRepository.getContacts().size())
             .filter(index -> contactRepository.getContacts().get(index).getId().equals(id))
             .findFirst()
-            .orElseThrow();
+            .orElseThrow(NoContactException::new);
     }
 
 }
--- Section 19 REST API/workbook8.1/contacts/src/main/java/com/ltp/contacts/web/ContactController.java
+++ Section 19 REST API/workbook8.1/contacts/src/main/java/com/ltp/contacts/web/ContactController.java
@@ -1,20 +1,14 @@
 package com.ltp.contacts.web;
 
-import java.util.List;
-
+import com.ltp.contacts.exception.NoContactException;
+import com.ltp.contacts.pojo.Contact;
+import com.ltp.contacts.service.ContactService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import com.ltp.contacts.pojo.Contact;
-import com.ltp.contacts.service.ContactService;
+import java.util.List;
 
 @RestController
 public class ContactController {
@@ -30,8 +24,13 @@ public class ContactController {
 
     @GetMapping("/contact/{id}")
     public ResponseEntity<Contact> getContact(@PathVariable String id) {
-        Contact contact = contactService.getContactById(id);
-        return new ResponseEntity<>(contact, HttpStatus.OK);
+        try {
+            Contact contact = contactService.getContactById(id);
+            return new ResponseEntity<>(contact, HttpStatus.OK);
+        } catch (NoContactException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
     }
     
     @PostMapping("/contact")
@@ -42,14 +41,23 @@ public class ContactController {
 
     @PutMapping("/contact/{id}")
     public ResponseEntity<Contact> updateContact(@PathVariable String id, @RequestBody Contact contact) {
-        contactService.updateContact(id, contact);   
-        return new ResponseEntity<Contact>(contactService.getContactById(id), HttpStatus.OK);
+        try {
+            contactService.updateContact(id, contact);
+            return new ResponseEntity<Contact>(contactService.getContactById(id), HttpStatus.OK);
+        } catch (NoContactException e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
     }
 
     @DeleteMapping("/contact/{id}")
     public ResponseEntity<HttpStatus> deleteContact(@PathVariable String id) {
-        contactService.deleteContact(id);
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        try {
+            contactService.deleteContact(id);
+            return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+        } catch (NoContactException e){
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
     }
 
 
