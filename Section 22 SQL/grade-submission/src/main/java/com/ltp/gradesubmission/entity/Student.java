607c0e030152b5d37e271537675d11cdd64b70b3
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-11-21 18:45:31 -0600

    SQL Video 176

--- Section 22 SQL/grade-submission/src/main/java/com/ltp/gradesubmission/entity/Student.java
+++ Section 22 SQL/grade-submission/src/main/java/com/ltp/gradesubmission/entity/Student.java
@@ -1,11 +1,17 @@
 package com.ltp.gradesubmission.entity;
 
+import javax.persistence.*;
 import java.time.LocalDate;
-
+@Entity
+@Table(name = "student")
 public class Student {
-
+    @Id
+    @GeneratedValue
+    @Column(name = "id")
     private Long id;
+    @Column(name="name", nullable = false)
     private String name;
+    @Column(name="birth_date", nullable = false)
     private LocalDate birthDate;
 
     public Long getId() {
