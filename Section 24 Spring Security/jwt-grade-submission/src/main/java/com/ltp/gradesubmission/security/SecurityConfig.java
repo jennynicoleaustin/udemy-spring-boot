55b29430b76f3586516857f3a06414bfbf2e42bd
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-11-24 18:37:23 -0600

    Spring Security: Video 216 - Complete - added AuthFilter

--- Section 24 Spring Security/jwt-grade-submission/src/main/java/com/ltp/gradesubmission/security/SecurityConfig.java
+++ Section 24 Spring Security/jwt-grade-submission/src/main/java/com/ltp/gradesubmission/security/SecurityConfig.java
@@ -1,6 +1,7 @@
 package com.ltp.gradesubmission.security;
 
 
+import com.ltp.gradesubmission.security.filter.AuthenticationFilter;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
@@ -17,12 +18,15 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        AuthenticationFilter authenticationFilter = new AuthenticationFilter();
+        authenticationFilter.setFilterProcessesUrl("/authenticate");
         http
                 .csrf().disable()
                 .authorizeRequests()
                 .antMatchers(HttpMethod.POST, SecurityConstants.REGISTER_PATH).permitAll() // Allows anyone to make post request on the path sign-up/register
                 .anyRequest().authenticated() // requires that all requests (other than antMatcher specified requests) be authenticated
                 .and()
+                .addFilter(authenticationFilter)
                 .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         return http.build();
     }
--- Section 24 Spring Security/jwt-grade-submission/src/main/java/com/ltp/gradesubmission/security/filter/AuthenticationFilter.java
+++ Section 24 Spring Security/jwt-grade-submission/src/main/java/com/ltp/gradesubmission/security/filter/AuthenticationFilter.java
@@ -1,5 +1,28 @@
 package com.ltp.gradesubmission.security.filter;
 
-public class AuthenticationFilter {
-    
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.ltp.gradesubmission.entity.User;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
+    //    /login
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        try {
+            User user = new ObjectMapper().readValue(request.getInputStream(), User.class);
+            // Above takes in the body of the request, deserializes it and objectMapper maps the deserialized information to a user object.
+            System.out.println(user.getUsername() + user.getPassword());
+        } catch (IOException e) {
+            throw new RuntimeException(); // you cannot handle an exception that occurs within a filter via the exception handler because authFilter occurs prior to the exception controller.
+        }
+
+        return super.attemptAuthentication(request, response);
+    }
+
 }
