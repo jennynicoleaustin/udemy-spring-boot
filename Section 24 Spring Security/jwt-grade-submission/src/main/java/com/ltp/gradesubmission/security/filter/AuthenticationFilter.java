b1a01eadd2cb32fa8aad6ceab222f4a8b36e88ad
Jenny Austin <109987252+jennynicoleaustin@users.noreply.github.com>
2022-11-25 20:00:32 -0600

    Spring Security: Video 220 - update gitignore - fixed flipped logic in authManager

--- Section 24 Spring Security/jwt-grade-submission/src/main/java/com/ltp/gradesubmission/security/filter/AuthenticationFilter.java
+++ Section 24 Spring Security/jwt-grade-submission/src/main/java/com/ltp/gradesubmission/security/filter/AuthenticationFilter.java
@@ -1,7 +1,10 @@
 package com.ltp.gradesubmission.security.filter;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ltp.gradesubmission.entity.User;
+import com.ltp.gradesubmission.security.SecurityConstants;
 import com.ltp.gradesubmission.security.manager.CustomAuthenticationManager;
 import lombok.AllArgsConstructor;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -14,6 +17,7 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Date;
 
 
 @AllArgsConstructor
@@ -33,15 +37,17 @@ public class AuthenticationFilter extends UsernamePasswordAuthenticationFilter {
         }
     }
 
-    @Override
-    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
-        System.out.println("NO GO ");
-    }
 
     @Override
     protected void successfulAuthentication(HttpServletRequest request, HttpServletResponse response, FilterChain chain, Authentication authResult) throws IOException, ServletException {
-        System.out.println("GO");
+        String token = JWT.create()
+                .withSubject(authResult.getName())
+                .withExpiresAt(new Date(System.currentTimeMillis() + SecurityConstants.TOKEN_EXPIRATION))
+                .sign(Algorithm.HMAC512(SecurityConstants.SECRET_KEY));
     }
 
-
+    @Override
+    protected void unsuccessfulAuthentication(HttpServletRequest request, HttpServletResponse response, AuthenticationException failed) throws IOException, ServletException {
+        System.out.println("FAILURE");
+    }
 }
--- Section 24 Spring Security/jwt-grade-submission/src/main/java/com/ltp/gradesubmission/security/manager/CustomAuthenticationManager.java
+++ Section 24 Spring Security/jwt-grade-submission/src/main/java/com/ltp/gradesubmission/security/manager/CustomAuthenticationManager.java
@@ -10,16 +10,19 @@ import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Component;
-@AllArgsConstructor
+
 @Component
+@AllArgsConstructor
 public class CustomAuthenticationManager implements AuthenticationManager {
     private UserService userService;
     private BCryptPasswordEncoder bCryptPasswordEncoder;
+
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
         User user = userService.getUser(authentication.getName());
-        if(bCryptPasswordEncoder.matches(authentication.getCredentials().toString(), user.getPassword())) {
-            throw new BadCredentialsException("Incorrect password"); }
+        if (!bCryptPasswordEncoder.matches(authentication.getCredentials().toString(), user.getPassword())) {
+            throw new BadCredentialsException("Incorrect password");
+        }
         return new UsernamePasswordAuthenticationToken(authentication.getName(), user.getPassword());
     }
 }
